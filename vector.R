7de66222f7567cff081d586d84ff2a5202226f27
unknown <dongwoo.jeong@outlook.com>
2022-02-19 23:55:55 -0500

    add vector1

---
+++ vector.R
@@ -0,0 +1,30 @@
+# vector
+
+# numeric, character, logical 
+c(1,2,3,4,5,6,7,8,9,10)
+c("we","love", "data", "analytics")
+c(TRUE,FALSE,TRUE,FALSE)
+
+# assign a vector into a variable
+odd <- c(1,3,5)
+even <- c(2,4,6)
+
+# combine two vectors
+# maintain the original order
+c(odd,even)
+
+# Create a vector with increasing or decreasing numbers
+3:9
+9:3
+seq(from=3, to=9)
+seq(from=3, to=9, by=2)
+seq(from=3.5, to=1.5, by=-0.5)
+seq(from=0, to=100, length.out=5)
+seq(from=-1, to=1, length.out=5)
+
+# replicate the value in x
+rep(1, times=3)
+rep(c(1,2,3), times=3)
+rep(c(1,2,3), each=3)
+rep(c(1,2,3), times=c(1,2,3))
+rep(c(1,2,3), length.out=8)
